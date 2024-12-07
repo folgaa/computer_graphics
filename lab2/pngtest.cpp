75d476bd03e748ccdbe162f495f59fd2babf7b9d
folgaa <fuga_olga@mail.ru>
2024-12-07 16:26:03 +0300

    Add 2 and 3 labs

--- lab2/pngtest.cpp
+++ lab2/pngtest.cpp
@@ -5,153 +5,249 @@
 #include <string.h>
 #include <stdio.h>
 #include <vector>
-#include <ctime>
-#include <iostream>
 
 using namespace std;
 
-// finding the nearest color 
-RGB NearestColor(RGB& pixelColor, int nBPP);
+// creating a palette with a specified number of bytes per channel
+vector<RGB> CreatePalette(int bpp);
+
+// finding the nearest color in the palette
+RGB NearestColor(const RGB& pixelColor, const vector<RGB>& palette);
 
 // processing image
-vector<vector<RGB>> processImage(vector<vector<RGB>> image, size_t width, size_t height, int nBPP);
+vector<vector<RGB>> processImage(vector<vector<RGB>> image, size_t width, size_t height, int levelsPerChannel);
 
 // creating a vector for convenient storage
 vector<vector<RGB>> get_rgb_vector(unsigned char* buffer, size_t width, size_t height);
 
 int main(int argc, char* argv[]) {
 
-	class CBitsPtrGuard {
-	public:
-		CBitsPtrGuard(unsigned char** pB)
-			: m_ppBits(pB) {}
-		~CBitsPtrGuard() {
-			if (*m_ppBits) delete* m_ppBits;
-		}
-	private:
-		unsigned char** m_ppBits;
-	};
+    class CBitsPtrGuard {
+    public:
+        CBitsPtrGuard(unsigned char** pB)
+            : m_ppBits(pB) {}
+        ~CBitsPtrGuard() {
+            if (*m_ppBits) delete* m_ppBits;
+        }
+    private:
+        unsigned char** m_ppBits;
+    };
 
-	// parse input parameters
-	char	szInputFileName[256];
-	char    szOutputFileName[256];
-	if (argc < 2)
-		printf("\nformat: pngtest <input_file> [<output_file>]");
-	else {
-		strcpy(szInputFileName, argv[1]);
-		if (argc > 2) {
-			strcpy(szOutputFileName, argv[2]);
-		}
-		else {
-			strcpy(szOutputFileName, szInputFileName);
-			strcat(szOutputFileName, "_1_out.png");
-		}
-	}
+    // parse input parameters
+    char  szInputFileName[256];
+    char    szOutputFileName[256];
+    if (argc < 2)
+        printf("\nformat: pngtest <input_file> [<output_file>]");
+    else {
+        strcpy(szInputFileName, argv[1]);
+        if (argc > 2) {
+            strcpy(szOutputFileName, argv[2]);
+        }
+        else {
+            strcpy(szOutputFileName, szInputFileName);
+            strcat(szOutputFileName, "_2_out.png");
+        }
+    }
 
-	size_t nReqSize = NPngProc::readPngFile(szInputFileName, 0, 0, 0, 0);
-	if (nReqSize == NPngProc::PNG_ERROR)
-	{
-		printf("\nError ocured while pngfile was read");
-		return -1;
-	}
 
-	unsigned char* pInputBits = new unsigned char[nReqSize];
-	if (!pInputBits)
-	{
-		printf("\nCan't allocate memory for image, required size is %u", nReqSize);
-		return -1;
-	}
-	CBitsPtrGuard InputBitsPtrGuard(&pInputBits);
+    size_t nReqSize = NPngProc::readPngFile(szInputFileName, 0, 0, 0, 0);
+    if (nReqSize == NPngProc::PNG_ERROR)
+    {
+        printf("\nError ocured while pngfile was read");
+        return -1;
+    }
 
-	size_t nWidth, nHeight;
-	unsigned int nBPP;
 
-	size_t nRetSize = NPngProc::readPngFile(szInputFileName, pInputBits, &nWidth, &nHeight, &nBPP);
+    unsigned char* pInputBits = new unsigned char[nReqSize];
+    if (!pInputBits)
+    {
+        printf("\nCan't allocate memory for image, required size is %u", nReqSize);
+        return -1;
+    }
+    CBitsPtrGuard InputBitsPtrGuard(&pInputBits);
 
-		vector<vector<RGB>> image = get_rgb_vector(pInputBits, nWidth, nHeight);
 
-		auto dithering_image = processImage(image, nWidth, nHeight, 1);
+    size_t nWidth, nHeight;
+    unsigned int nBPP;
 
-		if (NPngProc::write_png_file(szOutputFileName, dithering_image, nWidth, nHeight) == NPngProc::PNG_ERROR) {
-			perror("\nError ocuured during png file was written\n");
-			return -1;
-		}
+    size_t nRetSize = NPngProc::readPngFile(szInputFileName, pInputBits, &nWidth, &nHeight, &nBPP);
 
-		return 0;
+    vector<vector<RGB>> image = get_rgb_vector(pInputBits, nWidth, nHeight);
+
+    auto dithering_image = processImage(image, nWidth, nHeight, 2);
+
+    if (NPngProc::write_png_file(szOutputFileName, dithering_image, nWidth, nHeight) == NPngProc::PNG_ERROR) {
+        perror("\nError ocuured during png file was written\n");
+        return -1;
+    }
+
+    return 0;
 }
 
 vector<vector<RGB>> get_rgb_vector(unsigned char* buffer, size_t width, size_t height) {
-	vector<vector<RGB>> image(height, vector<RGB>(width));
+    vector<vector<RGB>> image(height, vector<RGB>(width));
 
-	// filling a two - dimensional vector with pixels from the buffer
-	for (size_t y = 0; y < height; ++y) {
-		for (size_t x = 0; x < width; ++x) {
-			// getting the index of the pixel in the buffer(each pixel consists of 3 bytes: R, G, B)
-			size_t index = (y * width + x) * 3;
+    // filling a two - dimensional vector with pixels from the buffer
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            // getting the index of the pixel in the buffer(each pixel consists of 3 bytes: R, G, B)
+            size_t index = (y * width + x) * 3;
 
-			// filling the RGB structure for the current pixel
-			image[y][x].r = buffer[index];
-			image[y][x].g = buffer[index + 1];
-			image[y][x].b = buffer[index + 2];
-		}
-	}
-
-	return image;
+            // filling the RGB structure for the current pixel
+            image[y][x].r = buffer[index];
+            image[y][x].g = buffer[index + 1];
+            image[y][x].b = buffer[index + 2];
+        }
+    }
+   
+    return image;
 }
 
-RGB NearestColor(RGB& pixelColor, int nBPP) {
+vector<RGB> CreatePalette(int bpp) {
+    vector<RGB> palette;
+    int colors = pow(2, bpp);
+    int step = 255 / (colors - 1);
 
-	pixelColor.r = pixelColor.r >> (8 - nBPP) << (8 - nBPP);
-	pixelColor.g = pixelColor.g >> (8 - nBPP) << (8 - nBPP);
-	pixelColor.b = pixelColor.b >> (8 - nBPP) << (8 - nBPP);
-
-	return pixelColor;
+    for (int r = 0; r < 256; r += step) {
+        for (int g = 0; g < 256; g += step) {
+            for (int b = 0; b < 256; b += step) {
+                palette.push_back({ static_cast<unsigned char>(r),
+                           static_cast<unsigned char>(g),
+                           static_cast<unsigned char>(b) });
+            }
+        }
+    }
+    return palette;
 }
 
-vector<vector<RGB>> processImage(vector<vector<RGB>> image, size_t width, size_t height, int nBPP) {
+RGB NearestColor(const RGB& pixelColor, const vector<RGB>& palette) {
+    RGB nearestColor = palette[0];
+    int minDistance = INT_MAX;
 
-	RGB newColor, error;
+    for (const RGB& color : palette) {
+        int distance =
+            (static_cast<int>(pixelColor.r) - static_cast<int>(color.r)) * (static_cast<int>(pixelColor.r) - static_cast<int>(color.r)) +
+            (static_cast<int>(pixelColor.g) - static_cast<int>(color.g)) * (static_cast<int>(pixelColor.g) - static_cast<int>(color.g)) +
+            (static_cast<int>(pixelColor.b) - static_cast<int>(color.b)) * (static_cast<int>(pixelColor.b) - static_cast<int>(color.b));
 
-	for (size_t y = 0; y < height - 1; ++y) {
-		for (size_t x = 1; x < width - 1; ++x) {
-			newColor = NearestColor(image[y][x], nBPP);
+        if (distance < minDistance) {
+            minDistance = distance;
+            nearestColor = color;
+        }
+    }
 
-			error.r = image[y][x].r - newColor.r;
-			error.g = image[y][x].g - newColor.g;
-			error.b = image[y][x].b - newColor.b;
+    return nearestColor;
+}
 
-			image[y][x].r = min(255, max(0, image[y][x].r - error.r));
-			image[y][x].g = min(255, max(0, image[y][x].g - error.g));
-			image[y][x].b = min(255, max(0, image[y][x].b - error.b));
-			
+int clamp(int value) {
+    return std::min(255, std::max(0, value));
+}
 
-			image[y][x + 1].r = min(255 ,max(0, image[y][x + 1].r + error.r * 7 / 16));
-			image[y][x + 1].g = min(255, max(0, image[y][x + 1].g + error.g * 7 / 16));
-			image[y][x + 1].b = min(255, max(0, image[y][x + 1].b + error.b * 7 / 16));
+vector<vector<RGB>> processImage(vector<vector<RGB>> image, size_t width, size_t height, int levelsPerChannel) {
+    vector<RGB> palette = CreatePalette(levelsPerChannel);
+    RGB newColor;
+    int error_r, error_g, error_b;
 
-			image[y + 1][x + 1].r = min(255, max(0, image[y + 1][x + 1].r + error.r / 16));
-			image[y + 1][x + 1].g = min(255, max(0, image[y + 1][x + 1].g + error.g / 16));
-			image[y + 1][x + 1].b = min(255, max(0, image[y + 1][x + 1].b + error.b / 16));
+    vector<vector<int>> new_image_r(height, vector<int>(width));
+    vector<vector<int>> new_image_g(height, vector<int>(width));
+    vector<vector<int>> new_image_b(height, vector<int>(width));
 
-			image[y + 1][x].r = min(255, max(0, image[y + 1][x].r + error.r * 5 / 16));
-			image[y + 1][x].g = min(255, max(0, image[y + 1][x].g + error.g * 5 / 16));
-			image[y + 1][x].b = min(255, max(0, image[y + 1][x].b + error.b * 5 / 16));
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            new_image_r[y][x] = static_cast<int>(image[y][x].r);
+            new_image_g[y][x] = static_cast<int>(image[y][x].g);
+            new_image_b[y][x] = static_cast<int>(image[y][x].b);
+        }
+    }
 
-			image[y + 1][x - 1].r = min(255, max(0, image[y + 1][x - 1].r + error.r * 3 / 16));
-			image[y + 1][x - 1].g = min(255, max(0, image[y + 1][x - 1].g + error.g * 3 / 16));
-			image[y + 1][x - 1].b = min(255, max(0, image[y + 1][x - 1].b + error.b * 3 / 16));
+    for (size_t y = 0; y < height - 1; ++y) {
+        for (size_t x = 1; x < width - 1; ++x) {
+            
+            // finding the nearest color
+            RGB currentColor = {
+                static_cast<unsigned char>(clamp(new_image_r[y][x])),
+                static_cast<unsigned char>(clamp(new_image_g[y][x])),
+                static_cast<unsigned char>(clamp(new_image_b[y][x]))
+            };
 
-		}
-	}
+            newColor = NearestColor(currentColor, palette);
 
-	for (size_t x = 0; x < width; x++) {
-		image[height - 1][x] = NearestColor(image[height - 1][x], nBPP);
-	}
+            new_image_r[y][x] = newColor.r;
+            new_image_g[y][x] = newColor.g;
+            new_image_b[y][x] = newColor.b;
 
-	for (size_t y = 0; y < height - 1; y++) {
-		image[y][0] = NearestColor(image[y][0], nBPP);
-		image[y][width-1] = NearestColor(image[y][width-1], nBPP);
-	}
+            // error calculation
+            error_r = static_cast<int>(currentColor.r) - static_cast<int>(newColor.r);
+            error_g = static_cast<int>(currentColor.g) - static_cast<int>(newColor.g);
+            error_b = static_cast<int>(currentColor.b) - static_cast<int>(newColor.b);
 
-	return image;
+            // Floyd-Steinberg dithering
+            new_image_r[y][x + 1] = new_image_r[y][x + 1] + static_cast<int>(error_r * 7.0 / 16);
+            new_image_g[y][x + 1] = new_image_g[y][x + 1] + static_cast<int>(error_g * 7.0 / 16);
+            new_image_b[y][x + 1] = new_image_b[y][x + 1] + static_cast<int>(error_b * 7.0 / 16);
+
+            new_image_r[y + 1][x + 1] = new_image_r[y + 1][x + 1] + static_cast<int>(error_r * 1.0 / 16);
+            new_image_g[y + 1][x + 1] = new_image_g[y + 1][x + 1] + static_cast<int>(error_g * 1.0 / 16);
+            new_image_b[y + 1][x + 1] = new_image_b[y + 1][x + 1] + static_cast<int>(error_b * 1.0 / 16);
+
+            new_image_r[y + 1][x] = new_image_r[y + 1][x] + static_cast<int>(error_r * 5.0 / 16);
+            new_image_g[y + 1][x] = new_image_g[y + 1][x] + static_cast<int>(error_g * 5.0 / 16);
+            new_image_b[y + 1][x] = new_image_b[y + 1][x] + static_cast<int>(error_b * 5.0 / 16);
+
+            new_image_r[y + 1][x - 1] = new_image_r[y + 1][x - 1] + static_cast<int>(error_r * 3.0 / 16);
+            new_image_g[y + 1][x - 1] = new_image_g[y + 1][x - 1] + static_cast<int>(error_g * 3.0 / 16);
+            new_image_b[y + 1][x - 1] = new_image_b[y + 1][x - 1] + static_cast<int>(error_b * 3.0 / 16);
+        }
+    }
+
+    // frame
+    for (size_t y = 0; y < height; y++) {
+        RGB currentColor = {
+                static_cast<unsigned char>(clamp(new_image_r[y][0])),
+                static_cast<unsigned char>(clamp(new_image_g[y][0])),
+                static_cast<unsigned char>(clamp(new_image_b[y][0]))
+        };
+
+        newColor = NearestColor(currentColor, palette);
+
+        new_image_r[y][0] = newColor.r;
+        new_image_g[y][0] = newColor.g;
+        new_image_b[y][0] = newColor.b;
+
+        currentColor = {
+                static_cast<unsigned char>(clamp(new_image_r[y][width - 1])),
+                static_cast<unsigned char>(clamp(new_image_g[y][width - 1])),
+                static_cast<unsigned char>(clamp(new_image_b[y][width - 1]))
+        };
+
+        newColor = NearestColor(currentColor, palette);
+
+        new_image_r[y][width - 1] = newColor.r;
+        new_image_g[y][width - 1] = newColor.g;
+        new_image_b[y][width - 1] = newColor.b;
+    }
+
+    for (size_t x = 1; x < width - 1; x++) {
+        RGB currentColor = {
+                static_cast<unsigned char>(clamp(new_image_r[height - 1][x])),
+                static_cast<unsigned char>(clamp(new_image_g[height - 1][x])),
+                static_cast<unsigned char>(clamp(new_image_b[height - 1][x]))
+        };
+
+        newColor = NearestColor(currentColor, palette);
+
+        new_image_r[height - 1][x] = newColor.r;
+        new_image_g[height - 1][x] = newColor.g;
+        new_image_b[height - 1][x] = newColor.b;
+    }
+
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            image[y][x].r = static_cast<unsigned char>(clamp(new_image_r[y][x]));
+            image[y][x].g = static_cast<unsigned char>(clamp(new_image_g[y][x]));
+            image[y][x].b = static_cast<unsigned char>(clamp(new_image_b[y][x]));
+        }
+    }
+
+    return image;
 }
---
+++ lab3/Graphics.cpp
@@ -0,0 +1,434 @@
+﻿#include "StdAfx.h"
+#include "GF.h"
+#include <vector>
+
+#ifndef M_PI
+const double M_PI = 3.1415926535897932384626433832795;
+#endif
+
+
+// Ваша реализация необходимых вам функций
+void DrawLine(int x1, int y1, int x2, int y2, RGBPIXEL color); // рисует отрезок
+
+void DrawPolygon(const std::vector<std::pair<int, int>>& vertices, RGBPIXEL color); // рисует полигон
+
+enum CLPointType { LEFT, RIGHT, BEYOND, BEHIND, BETWEEN, ORIGIN, DESTINATION }; // классификация положения точки отн-но отрезка
+
+CLPointType Classify(int x1, int y1, int x2, int y2, int x, int y); // функция для определения положения точки отн-но отрезка
+
+bool IsConvexPolygon(const std::vector<std::pair<int, int>>& points); // выпуклый ли полигон
+
+enum IntersectType { SAME, PARALLEL, SKEW, SKEW_CROSS, SKEW_NO_CROSS }; // классификация расположения двух отрезков
+
+IntersectType Intersect(int ax, int ay, int bx, int by, int cx, int cy,
+    int dx, int dy, double* t); // функция для определения расположения двух отрезков
+
+IntersectType Cross(int ax, int ay, int bx, int by, int cx, int cy,
+    int dx, int dy, double* tab, double* tcd); // функция для определения наличия пересечения двух отрезков
+
+bool IsSimplePolygon(const std::vector<std::pair<int, int>>& vertices); // является ли полигон простым
+
+enum EType { TOUCHING, CROSS_LEFT, CROSS_RIGHT, INESSENTIAL }; // классификация стороны полигона отн-но луча
+
+EType EdgeType(int ox, int oy, int dx, int dy, int ax, int ay); // функция для определения положения луча отн-но стороны полигона
+
+enum PType { INSIDE, OUTSIDE }; // классификация точек отн-но полигона
+
+PType PInPolygonEOMode(int x, int y,
+    const std::vector<std::pair<int, int>>& vertices,
+    int n); // определение принадлежности точки полигону по правилу Even-Odd
+
+void FillPolygonEvenOdd(const std::vector<std::pair<int, int>>& vertices, int n, RGBPIXEL color); // закраска полигона
+
+PType PInPolygonNZWMode(int x, int y,
+    const std::vector<std::pair<int, int>>& vertices,
+    int n); // определение принадлежности точки полигону по правилу Non-Zero Winding
+
+void FillPolygonNonZeroWinding(const std::vector<std::pair<int, int>>& vertices, int n, RGBPIXEL color); // закраска полигона
+
+// Вызывается один раз в самом начале при инициализации приложения
+bool gfInitScene()
+{
+    gfSetWindowSize( 640, 480 );
+    
+
+    /*std::vector<std::pair<int, int>> polygon = {
+    {200, 50},   // A
+    {150, 400},  // B
+    {100, 200},  // C
+    {300, 100},  // D
+    {350, 250},  // E
+    {50, 350},   // F
+    {250, 200}   // G
+    };*/
+
+    std::vector<std::pair<int, int>> polygon = {
+    {200, 50},   // A
+    {300, 50},  // B
+    {300, 150},  // C
+    {200, 150},  // D
+    };
+
+
+    DrawPolygon(polygon, RGBPIXEL::Yellow());
+
+    // FillPolygonEvenOdd(polygon, 4, RGBPIXEL::Cyan());
+    // FillPolygonNonZeroWinding(polygon, 4, RGBPIXEL::Cyan());
+
+    const TCHAR* message = IsConvexPolygon(polygon) ? _T("Полигон выпуклый.") : _T("Полигон невыпуклый.");
+
+    const TCHAR* message2 = IsSimplePolygon(polygon) ? _T("Полигон простой.") : _T("Полигон сложный.");
+
+    gfDisplayMessage(message);
+
+    gfDisplayMessage(message2);
+
+    return true;
+}
+
+// Вызывается в цикле до момента выхода из приложения.
+// Следует использовать для создания анимационных эффектов
+void gfDrawScene()
+{
+    //gfClearScreen(RGBPIXEL::Black());
+
+    //static int x = 0;
+    //gfDrawRectangle(x, 100, x + 50, 130, RGBPIXEL::Blue());
+    //x = (x + 1) % gfGetWindowWidth() ;
+
+    //int x = gfGetMouseX(),
+    //    y = gfGetMouseY();
+    //gfDrawRectangle(x - 10, y - 10, x + 10, y + 10, RGBPIXEL::Green());
+}
+
+// Вызывается один раз перед выходом из приложения.
+// Следует использовать для освобождения выделенных
+// ресурсов (памяти, файлов и т.п.)
+void gfCleanupScene()
+{
+}
+
+// Вызывается когда пользователь нажимает левую кнопку мыши
+void gfOnLMouseClick( int x, int y )
+{
+    x; y;
+    gfDrawRectangle(x - 10, y - 10, x + 10, y + 10, RGBPIXEL::Green());
+}
+
+// Вызывается когда пользователь нажимает правую кнопку мыши
+void gfOnRMouseClick( int x, int y )
+{
+    x; y;
+}
+
+// Вызывается когда пользователь нажимает клавишу на клавиатуре
+void gfOnKeyDown( UINT key )
+{
+    key;
+
+    if( key == 'A' )
+        gfDisplayMessage( "'A' key has been pressed" );
+}
+
+// Вызывается когда пользователь отжимает клавишу на клавиатуре
+void gfOnKeyUp( UINT key )
+{
+    key;
+
+    //if( key == 'B' )
+    //    gfDisplayMessage( "'B' key has been un-pressed" );
+}
+
+// Мои функции
+
+void DrawLine(int x1, int y1, int x2, int y2, RGBPIXEL color)
+{
+    int x = x1, y = y1;
+    int dx = x2 - x1, dy = y2 - y1;
+    int ix, iy;
+    int e;
+    int i;
+
+    if (dx > 0)
+        ix = 1;
+    else if (dx < 0)
+    {
+        ix = -1;
+        dx = -dx;
+    }
+    else
+        ix = 0;
+
+    if (dy > 0)
+        iy = 1;
+    else if (dy < 0)
+    {
+        iy = -1;
+        dy = -dy;
+    }
+    else
+        iy = 0;
+
+    if (dx >= dy)
+    {
+        e = 2 * dy - dx;
+        for (i = 0; i <= dx; i++)
+        {
+            gfSetPixel(x, y, color);
+            if (e >= 0)
+            {
+                y += iy;
+                e -= 2 * dx;
+            }
+            x += ix;
+            e += 2 * dy;
+        }
+    }
+    else
+    {
+        e = 2 * dx - dy;
+        for (i = 0; i <= dy; i++)
+        {
+            gfSetPixel(x, y, color);
+            if (e >= 0)
+            {
+                x += ix;
+                e -= 2 * dy;
+            }
+            y += iy;
+            e += 2 * dx;
+        }
+    }
+
+}
+
+void DrawPolygon(const std::vector<std::pair<int, int>>& vertices, RGBPIXEL color)
+{
+    size_t n = vertices.size();
+    if (n < 3) return; // нечего рисовать, если меньше 3 вершин
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        int x1 = vertices[i].first;
+        int y1 = vertices[i].second;
+        int x2 = vertices[(i + 1) % n].first;
+        int y2 = vertices[(i + 1) % n].second;
+        DrawLine(x1, y1, x2, y2, color);
+    }
+}
+
+CLPointType Classify(int x1, int y1, int x2, int y2, int x, int y) {
+    int ax = x2 - x1; //a
+    int ay = y2 - y1;
+    int bx = x - x1; //b
+    int by = y - y1;
+    int s = ax * by - bx * ay;
+    if (s > 0) return LEFT;
+    if (s < 0) return RIGHT;
+    if ((ax * bx < 0) || (ay * by < 0)) //противоположно направлению
+        return BEHIND; //позади
+    if ((ax * ax + ay * ay) < (bx * bx + by * by))
+        return BEHIND; //впереди
+    if (x1 == x && y1 == y) //совпадает с началом
+        return ORIGIN;
+    if (x2 == x && y2 == y) //совпадает с концом
+        return DESTINATION;
+    return BETWEEN; //между
+}
+
+bool IsConvexPolygon(const std::vector<std::pair<int, int>>& points) {
+    if (points.size() < 3) return false; // Полигон должен иметь минимум 3 вершины
+
+    int n = points.size();
+    CLPointType globalSide = ORIGIN; // Начальное положение (нет направления)
+
+    // Проверяем все стороны полигона
+    for (int i = 0; i < n; i++) {
+        // Вершины текущей стороны
+        int x1 = points[i].first;
+        int y1 = points[i].second;
+        int x2 = points[(i + 1) % n].first;
+        int y2 = points[(i + 1) % n].second;
+
+        CLPointType localSide = ORIGIN; // Направление для текущей стороны
+
+        // Проверяем все остальные вершины
+        for (int j = 0; j < n; j++) {
+            if (j == i || j == (i + 1) % n) continue; // Пропускаем вершины стороны
+
+            int x = points[j].first;
+            int y = points[j].second;
+
+            CLPointType type = Classify(x1, y1, x2, y2, x, y);
+
+            // Учитываем только положения LEFT и RIGHT
+            if (type == LEFT || type == RIGHT) {
+                if (localSide == ORIGIN) {
+                    localSide = type; // Устанавливаем направление
+                }
+                else if (localSide != type) {
+                    return false; // Вершины оказались с разных сторон
+                }
+            }
+        }
+
+        // Устанавливаем глобальное направление
+        if (globalSide == ORIGIN) {
+            globalSide = localSide;
+        }
+        else if (globalSide != localSide) {
+            return false; // Направление изменилось для другой стороны
+        }
+    }
+
+    return true; // Полигон выпуклый
+}
+
+IntersectType Intersect(int ax, int ay, int bx, int by, int cx, int cy,
+    int dx, int dy, double* t) {
+    int nx = dy - cy; //вычисление координат n
+    int ny = cx - dx;
+    CLPointType type;
+    double denom = nx * (bx - ax) + ny * (by - ay); //n ∗ b − a
+    if (denom == 0) { //параллельны или совпадают
+        type = Classify(cx, cy, dx, dy, ax, ay); // положение точки a относительно прямой cd
+        if (type == LEFT || type == RIGHT)
+            return PARALLEL;
+        else return SAME;
+    }
+    double num = nx * (ax - cx) + ny * (ay - cy); //n ∗ a − c
+    *t = -num / denom; // по значению t можно сделать вывод о пересечении отрезка ab
+    return SKEW;
+}
+
+IntersectType Cross(int ax, int ay, int bx, int by, int cx, int cy,
+    int dx, int dy, double* tab, double* tcd) {
+    IntersectType type = Intersect(ax, ay, bx, by, cx, cy, dx, dy, tab);
+    if (type == SAME || type == PARALLEL)
+        return type;
+    if ((*tab < 0) || (*tab > 1))
+        return SKEW_NO_CROSS;
+    Intersect(cx, cy, dx, dy, ax, ay, bx, by, tcd);
+    if ((*tcd < 0) || (*tcd > 1))
+        return SKEW_NO_CROSS;
+    return SKEW_CROSS;
+}
+
+bool IsSimplePolygon(const std::vector<std::pair<int, int>>& vertices) {
+    int n = vertices.size();
+
+    for (int i = 0; i < n; ++i) {
+        int ax = vertices[i].first, ay = vertices[i].second;
+        int bx = vertices[(i + 1) % n].first, by = vertices[(i + 1) % n].second;
+
+        for (int j = i + 2; j < n; ++j) {
+            int cx = vertices[j].first, cy = vertices[j].second;
+            int dx = vertices[(j + 1) % n].first, dy = vertices[(j + 1) % n].second;
+
+            if (i == 0 && j == n - 1) continue;
+
+            double tab, tcd;
+            IntersectType type = Cross(ax, ay, bx, by, cx, cy, dx, dy, &tab, &tcd);
+
+            if (type == SKEW_CROSS) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+EType EdgeType(int ox, int oy, int dx, int dy, int ax, int ay) {
+    switch (Classify(ox, oy, dx, dy, ax, ay)) {
+    case LEFT:
+        if (ay > oy && ay <= dy) return CROSS_LEFT; // пересекающая, A слева
+        else return INESSENTIAL; // безразличная
+    case RIGHT:
+        if (ay > dy && ay <= oy) return CROSS_RIGHT; // пересекающая, A справа
+        else return INESSENTIAL; // безразличная
+    case BETWEEN:
+    case ORIGIN:
+    case DESTINATION:
+        return TOUCHING; // касающаяся
+    default:
+        return INESSENTIAL; // безразличная
+    }
+}
+
+PType PInPolygonEOMode(int x, int y, //точка
+    const std::vector<std::pair<int, int>>& vertices, //координаты полигона
+    int n) { //количество вершин в полигоне
+    int param = 0;
+    for (int i = 0; i < n; i++) {
+        switch (EdgeType(vertices[i].first, vertices[i].second, vertices[(i + 1) % n].first, vertices[(i + 1) % n].second, x, y)) {
+        case TOUCHING: //если лежит на полигоне, то заведомо принадлежит
+            return INSIDE;
+        case CROSS_LEFT:
+        case CROSS_RIGHT:
+            param = 1 - param; //изменяем значение четности
+        }
+    }
+    if (param == 1) return INSIDE; //нечетное
+    else return OUTSIDE;
+}
+
+void FillPolygonEvenOdd(const std::vector<std::pair<int, int>>& vertices, int n, RGBPIXEL color) {
+
+    int max_x = 0, max_y = 0;
+    int min_x = 640, min_y = 480;
+    for (int i = 0; i < n; i++) {
+        if (vertices[i].first > max_x) max_x = vertices[i].first;
+        if (vertices[i].first < min_x) min_x = vertices[i].first;
+        if (vertices[i].second > max_y) max_y = vertices[i].second;
+        if (vertices[i].second < min_y) min_y = vertices[i].second;
+    }
+
+    for (int y = min_y; y <= max_y; y++) {
+        for (int x = min_x; x <= max_x; x++) {
+            if (PInPolygonEOMode(x, y, vertices, n) == INSIDE) {
+                gfSetPixel(x, y, color);
+            }
+        }
+    }
+}
+
+PType PInPolygonNZWMode(int x, int y, //точка
+    const std::vector<std::pair<int, int>>& vertices, //координаты полигона
+    int n) { //количество вершин в полигоне
+    int param = 0;
+    for (int i = 0; i < n; i++) {
+        switch (EdgeType(vertices[i].first, vertices[i].second, vertices[(i + 1) % n].first, vertices[(i + 1) % n].second, x, y)) {
+        case TOUCHING: //если лежит на полигоне, то заведомо принадлежит
+            return INSIDE;
+        case CROSS_LEFT:
+            param++;
+            break;
+        case CROSS_RIGHT:
+            param--;
+            break;
+        }
+    }
+    if (param != 0) return INSIDE;
+    else return OUTSIDE;
+}
+
+void FillPolygonNonZeroWinding(const std::vector<std::pair<int, int>>& vertices, int n, RGBPIXEL color) {
+
+    int max_x = 0, max_y = 0;
+    int min_x = 640, min_y = 480;
+    for (int i = 0; i < n; i++) {
+        if (vertices[i].first > max_x) max_x = vertices[i].first;
+        if (vertices[i].first < min_x) min_x = vertices[i].first;
+        if (vertices[i].second > max_y) max_y = vertices[i].second;
+        if (vertices[i].second < min_y) min_y = vertices[i].second;
+    }
+
+    for (int y = min_y; y <= max_y; y++) {
+        for (int x = min_x; x <= max_x; x++) {
+            if (PInPolygonNZWMode(x, y, vertices, n) == INSIDE) {
+                gfSetPixel(x, y, color);
+            }
+        }
+    }
+}
